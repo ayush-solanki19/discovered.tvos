5f998f32bc11aa99126568da82057fada0a73ae7
ayush-solanki19 <ayush.solanki@pixelnx.com>
2026-08-14 12:25:27 +0530

    Initial Commit

---
+++ discovered.tvosTests/discovered_tvosTests.swift
@@ -0,0 +1,17 @@
+//
+//  discovered_tvosTests.swift
+//  discovered.tvosTests
+//
+//  Created by mac mini on 14/08/26.
+//
+
+import Testing
+@testable import discovered_tvos
+
+struct discovered_tvosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
